443109d0d8031647e55f95946374f257ca2a4b62
Arne Neumann <github@arne.cl>
2014-07-21 21:38:32 +0200

    added filters and preferences modules w/ function stubs

---
+++ src/pocores/filters.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+
+"""
+The ``filters`` module provides functions that filter the set of antecedent
+candidates.
+"""
+
+import preferences
+
+def get_filtered_candidates(pocores, cand_list, (sent, word), sentence_dist,
+                            verbose=False):
+    """
+    applies several filters to the list of potential antecedents.
+
+    Parameters
+    ----------
+    pocores : Pocores
+        an instance of the Pocores class
+    cand_list : list of (int, int) tuples
+        a list of potential antecedents, represented as
+        (sentence index, word index) tuples
+    (sent, word) : tuple of (int, int)
+        the (sentence index, word index tuple) of the anaphora
+    sentence_dist : int
+        number of preceding sentences that will be looked at, i.e. the
+        sentences that contain potential antecedents
+
+    Returns
+    -------
+    filtered candidates : list of (int, int) tuples
+        list of likely antecedents, represented as
+        (sentence index, word index) tuples
+    """
+    raise NotImplementedError
+
+def check_agreement(pocores, antecedent, anaphora):
+    """
+    Checks if an anaphora and a potential antecedent have morphological
+    agreement.
+    If the anaphora is a possessive, only the features person and gender are
+    checked, otherwise gender, person and number.
+
+    :param pocores: an instance of the Pocores class
+    :type pocores: ``Pocores``
+    :param antecedent: the antecedent's (sentence index, word index) tuple
+    :type antecedent: ``tuple`` of (``int``, ``int``)
+    :param anaphora: the anaphora's (sentence index, word index) tuple
+    :type anaphora: ``tuple`` of (``int``, ``int``)
+
+    :return: True, if antecedent and anaphora agree in gender,
+    person (and number); False otherwise.
+    :rtype: ``bool``
+    """
+    def check_feat(antecedent_dict, anaphora_dict, feat): #TODO add doctest
+        """
+        Checks if anaphora and antecedent share the same feature. The
+        principle of underspecification is used, i.e. only if both words have a
+        certain feature and disagree in it the agreement check is False.
+
+        :param antecedent_dict: the feature dictionary of the antecedent
+        :type antecedent_dict: ``dict`` with ``str`` keys and ``str`` or ``int``
+         values
+        :param anaphora_dict: the feature dictionary of the anaphora
+        :type anaphora_dict: ``dict`` with ``str`` keys and ``str`` or ``int``
+         values
+        :return: False, if antecedent and anaphora disagree in a feature;
+        True otherwise.
+        :rtype: ``bool``
+        """
+        raise NotImplementedError
+    raise NotImplementedError
+
+def check_binding(pocores, antecedent, anaphor):
+    """
+    Checks if two words can be anaphora and antecedent by the binding
+    principles of chomsky.
+    The binding category is considered the (sub)clause of the anaphora.
+
+    :param pocores: an instance of the Pocores class
+    :type pocores: ``Pocores``
+    :param antecedent: the antecedent's (sentence index, word index) tuple
+    :type antecedent: ``tuple`` of (``int``, ``int``)
+    :param anaphora: the anaphora's (sentence index, word index) tuple
+    :type anaphora: ``tuple`` of (``int``, ``int``)
+
+    :return: True, if antecedent can be bound by anaphora; False otherwise.
+    :rtype: ``bool``
+    """
+    def get_binding_category(a): #TODO: describe binding categories better
+        """
+        Grammatical notions used by this function:
+
+            - Deprel: dependency relation to the HEAD. the set of possible
+            deprels depends on the original treebank annotation
+                - PUNC: punctuation ("," or sentence final ".")
+                - CD: relation involving conjunctions (e.g. und, doch, aber)?
+        """
+        raise NotImplementedError
+    raise NotImplementedError
+
+def check_semantik(pocores, antecedent, anaphor):
+    """
+    Placeholder
+    """
+    raise NotImplementedError
+
+def check_coreference(pocores, antecedent, anaphora):
+    """
+    So far: checks if two words have the same lemma.
+
+    :param pocores: an instance of the Pocores class
+    :type pocores: ``Pocores``
+    :param antecedent: the antecedent's (sentence index, word index) tuple
+    :type antecedent: ``tuple`` of (``int``, ``int``)
+    :param anaphora: the anaphora's (sentence index, word index) tuple
+    :type anaphora: ``tuple`` of (``int``, ``int``)
+
+    :return: True, if antecedent and anaphora share the same lemma; False
+    otherwise.
+    :rtype: ``bool``
+    """
+    raise NotImplementedError
+
+def is_expletive(pocores, anaphora):
+    """
+    Checks if a given Word is an expletive.
+
+    :param pocores: an instance of the Pocores class
+    :type pocores: ``Pocores``
+    :param anaphora: the anaphora's (sentence index, word index) tuple
+    :type anaphora: ``tuple`` of (``int``, ``int``)
+    """
+    raise NotImplementedError
---
+++ src/pocores/preferences.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+def check_parallelism(pocores, (sent1, word1), (sent2, word2)):
+    """
+    Checks syntactical role parallelism between two given words.
+
+    :param (sent1, word1): a word index
+    :type (sent1, word1): (int, int) ``tuple``
+    :param (sent2, word2): a word index
+    :type (sent2, word2): (int, int) ``tuple``
+
+    :return: True if parallelism is found, False otherwise.
+    :rtype: ``boolean``
+    """
+    raise NotImplementedError
+
+def get_distance((sent1, word1), (sent2, word2)):
+    """
+    Returns sentence distance between to given words.
+
+    >>> get_distance((1, 2), (5, 4))
+    4
+    >>> get_distance((1, 2), (1, 9))
+    0
+    """
+    raise NotImplementedError
+
+def check_role(pocores, antecedent, role):
+    """
+    Checks if a given word has a given syntactic role.
+    """
+    raise NotImplementedError
+
+def get_chain_length(pocores, antecedent):
+    """
+    Returns the count of known mentions of the discourse referent of the
+    given word.
+    """
+    raise NotImplementedError
+
+def get_depth(pocores, (sent, word)):
+    """
+    Returns number of dependency edges from given word to root of the
+    sentence.
+    """
+    raise NotImplementedError
