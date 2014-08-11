c5ae23e2efa365787d75c2cfdff66d2565a6901e
Arne Neumann <github@arne.cl>
2014-08-11 13:35:52 +0200

    added _get_candidates()

--- src/pocores/pocores.py
+++ src/pocores/pocores.py
@@ -4,6 +4,7 @@
 
 import sys
 
+from discoursegraphs.util import natural_sort_key
 from discoursegraphs.readwrite import ConllDocumentGraph
 
 class Pocores(object):
@@ -34,6 +35,23 @@ class Pocores(object):
         # entity grids (key: int sentence, value XXX
         # TODO: write entity grid description
         self.entity_grid = {}
+
+    def _get_candidates(self):
+        """
+        Returns list of all known discourse entities.
+
+        TODO: implement `self.entities`, issue #2
+        TODO: check if natural_sort_key also works for ``s1_t23``-like keys
+
+        Returns
+        -------
+        candidates : list of str
+            a sorted list of token node IDs
+        """
+        candidates = []
+        for entity_node_id in self.entities:
+            candidates += self.entities[entity_node_id]
+        return sorted(candidates, key=natural_sort_key)
 
 
 
