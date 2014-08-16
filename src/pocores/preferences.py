dc1a8485bb5f5418575b7ee598487a1d7d76be6c
Arne Neumann <github@arne.cl>
2014-08-16 22:42:56 +0200

    preferences: (re)implemented check_parallelism()

--- src/pocores/preferences.py
+++ src/pocores/preferences.py
@@ -3,19 +3,32 @@
 
 from discoursegraphs.readwrite.conll import traverse_dependencies_up
 
-def check_parallelism(pocores, (sent1, word1), (sent2, word2)):
+
+def check_parallelism(pocores, antecedent_id, anaphora_id,
+                      deprel_attrib='pdeprel'):
     """
     Checks syntactical role parallelism between two given words.
 
-    :param (sent1, word1): a word index
-    :type (sent1, word1): (int, int) ``tuple``
-    :param (sent2, word2): a word index
-    :type (sent2, word2): (int, int) ``tuple``
+    Parameters
+    ----------
+    pocores : Pocores
+        an instance of the Pocores class
+    antecedent_id : str
+        the node ID of the antecedent
+    anaphora_id : str
+        the node ID of the anaphora
 
-    :return: True if parallelism is found, False otherwise.
-    :rtype: ``boolean``
+    Returns
+    -------
+    parallel : bool
+    True, if parallelism is found, False otherwise.
     """
-    raise NotImplementedError
+    ant = pocores.node_attrs(antecedent_id)
+    ana = pocores.node_attrs(anaphora_id)
+    if (ant[deprel_attrib] == ana[deprel_attrib]
+       and ant[deprel_attrib] in ("SB", "OA", "DA")):
+        return True
+    return False
 
 
 def check_role(pocores, antecedent, role):
