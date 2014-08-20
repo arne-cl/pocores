a01d5ee2c36b81609f8b4d752b56e82a09196d00
Arne Neumann <github@arne.cl>
2014-08-20 10:54:53 +0200

    fixed setup.py; "converted" README to .rst

--- setup.py
+++ setup.py
@@ -1,41 +1,53 @@
-import os
-import sys
+# This is your "setup.py" file.
+# See the following sites for general guide to Python packaging:
+#   * `The Hitchhiker's Guide to Packaging <http://guide.python-distribute.org/>`_
+#   * `Python Project Howto <http://infinitemonkeycorps.net/docs/pph/>`_
+
 from setuptools import setup, find_packages
-import platform
+import sys, os
 
 here = os.path.abspath(os.path.dirname(__file__))
-#README = open(os.path.join(here, 'README.rst')).read()
+README = open(os.path.join(here, 'README.rst')).read()
 #NEWS = open(os.path.join(here, 'NEWS.rst')).read()
+
 
 version = '0.1.0'
 
-install_requires = ["discoursegraphs"]
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    "discoursegraphs", "unidecode", "brewer2mpl"
+]
 
 
 setup(name='pocores',
     version=version,
-    description="graph-based coreference resolution for German",
-    #~ long_description=README + '\n\n' + NEWS,
+    description="coreference resolution for German",
+    #long_description=README + '\n\n' + NEWS,
+    long_description=README,
     # Get classifiers from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[c.strip() for c in """
-        Development Status :: 2 - Pre-Alpha
-        License :: OSI Approved :: GNU General Public License v3 (GPLv3)
+        Development Status :: 3 - Alpha
+        License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)
         Operating System :: OS Independent
         Programming Language :: Python :: 2.7
+        Topic :: Software Development :: Libraries :: Python Modules
+        Natural Language :: German
         Topic :: Text Processing :: Linguistic
-    """.split('\n') if c.strip()],
+        """.split('\n') if c.strip()],
     keywords='linguistics nlp coreference anaphora german',
     author='Arne Neumann',
     author_email='pocores.programming@arne.cl',
     url='https://github.com/arne-cl/pocores',
-    license='GPL Version 3',
-    packages=['pocores'],
-    package_dir = {'pocores': "src/pocores"},
-    #~ package_data = {'pocores': ['data/*', 'grammar/*', 'avm.sty']},
+    license='AGPLv3+',
+    packages=find_packages("src"),
+    package_dir = {'': "src"},
+    include_package_data=True,
     zip_safe=False,
     install_requires=install_requires,
-    #~ entry_points={
-        #~ 'console_scripts':
-            #~ ['pocores=pocores.pocores:main']
-    #~ }
+    entry_points={
+        'console_scripts':
+            ['pocores=pocores.main:run_pocores_with_cli_arguments']
+    }
 )
